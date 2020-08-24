7f114321fa742e9bc15a32b5a871e890532585e5
Daniel Sun <danielsun3164@gmail.com>
2020-08-25 01:49:30 +0900

    practiceを追加

--- practice/test/test_practiceA.cpp
+++ practice/test/test_practiceA.cpp
@@ -3,12 +3,14 @@
 
 #include "command.h"
 
-static const std::string COMMAND = "./practiceA";
+using namespace std;
+
+static const string COMMAND = "./practiceA";
 
 TEST(practiceA, case1) {
-	check(COMMAND, std::string("") + "1\n" + "2 3" + "test\n", "6 test");
+	check(COMMAND, string("") + "1\n" + "2 3" + "test", string("") + "6 test");
 }
 
 TEST(practiceA, case2) {
-	check(COMMAND, std::string("") + "72\n" + "128 256\n" + "myonmyon", "456 myonmyon");
+	check(COMMAND, string("") + "72\n" + "128 256\n" + "myonmyon", string("") + "456 myonmyon");
 }
