f48b53af19004c8e6c5f4acc358478dc6d3624fd
Daniel Sun <danielsun3164@gmail.com>
2021-01-24 23:34:44 +0900

    abc001を追加

---
+++ abc/abc001-050/test/abc001/test_abc001B.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <command.h>
+
+using namespace std;
+
+static const string COMMAND = "./abc001B";
+
+TEST(abc001B, case1) {
+	check(COMMAND, string("") + "15000", string("") + "65");
+}
+
+TEST(abc001B, case2) {
+	check(COMMAND, string("") + "75000", string("") + "89");
+}
+
+TEST(abc001B, case3) {
+	check(COMMAND, string("") + "200", string("") + "02");
+}
