90e58408eca952b2612744912976b0d63c9009ce
Daniel Sun <danielsun3164@gmail.com>
2021-12-10 02:44:59 +0900

    コンパイルエラーを解消

--- other/joi2006yo/src/problemE.cpp
+++ other/joi2006yo/src/problemE.cpp
@@ -2,7 +2,7 @@
 #include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
 
-namespace mp = boost::multiprecision;
+namespace bmp = boost::multiprecision;
 
 /*
  * https://atcoder.jp/contests/joi2006yo/submissions/13554106 を参考に作成
@@ -11,10 +11,10 @@ int main() {
 	int n, k, m, r;
 	cin >> n >> k >> m >> r;
 	if (0 == m) {
-		mp::cpp_int x = 10, y = mp::pow(x, r), z = y / n;
+		bmp::cpp_int x = 10, y = bmp::pow(x, r), z = y / n;
 		cout << z / y << "." << setw(r) << setfill('0') << z % y << endl;
 	} else {
-		mp::cpp_int x = 10, y = mp::pow(x, r), z = y;
+		bmp::cpp_int x = 10, y = bmp::pow(x, r), z = y;
 		for (int i = 1; i < n; i++) {
 			z += y / i;
 		}
